05af6af81f28c5a085809bd85fc296abfd3080bd
Eric Closquet <closquet.eric@live.be>
2019-07-13 17:01:19 +0200

    add guzzle and passport packages

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace aleafoodapi\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,17 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes(function ($router) {
+            $router->forAccessTokens();
+//            $router->forPersonalAccessTokens();
+//            $router->forTransientTokens();
+        });
 
+        Passport::tokensExpireIn(now()->addMinutes(1000000));
+
+        Passport::refreshTokensExpireIn(now()->addDays(10));
+
+        Passport::personalAccessTokensExpireIn(now()->addMinutes(10));
         //
     }
 }
