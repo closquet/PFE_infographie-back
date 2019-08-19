b5b9bfd1ed7189115a6e84b01bc50f39a424e0c9
Eric Closquet <closquet.eric@live.be>
2019-08-19 21:39:32 +0200

    edit "crea ingredien_recipe table" migration: add detail, amount, measure and primary ingredient_id/recipe_id
    Signed-off-by: Eric Closquet <closquet.eric@live.be>

--- database/migrations/2019_07_25_220429_create_ingredient_recipe_table.php
+++ database/migrations/2019_07_25_220429_create_ingredient_recipe_table.php
@@ -16,7 +16,11 @@ class CreateIngredientRecipeTable extends Migration
         Schema::create('ingredient_recipe', function (Blueprint $table) {
             $table->bigInteger('ingredient_id')->unsigned()->index();
             $table->bigInteger('recipe_id')->unsigned()->index();
+            $table->string('detail')->nullable();
+            $table->float('amount')->unsigned();
+            $table->string('measure');
             $table->timestamps();
+            $table->primary(['ingredient_id', 'recipe_id']);
             $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
             $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
         });
